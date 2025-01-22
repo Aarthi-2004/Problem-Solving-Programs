5e94706bbcf1f64d5f1c7c848ce3e3e14a9dc512
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-01-22 22:18:16 +0530

    Add files via upload
    This Java program demonstrates how to compare two floating-point numbers (num1 and num2) using conditional statements. It evaluates whether one number is greater than, less than, or equal to the other and prints the result. The program highlights the use of if-else logic for decision-making and showcases formatted output to display meaningful comparisons. It serves as a simple and clear example for beginners to understand numeric comparisons and basic syntax in Java.

---
+++ FloatComparison.java
@@ -0,0 +1,14 @@
+class FloatComparison {
+    public static void main(String[] args) {
+        float num1 = 4.206f;
+        float num2 = 6.459f;
+
+        if (num1 > num2) {
+            System.out.println(num1 + " is greater than " + num2);
+        } else if (num1 < num2) {
+            System.out.println(num1 + " is less than " + num2);
+        } else {
+            System.out.println(num1 + " is equal to " + num2);
+        }
+    }
+}
