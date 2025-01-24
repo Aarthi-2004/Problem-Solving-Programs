9f7054473a812c23532d705d14f39c2a827acec1
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-01-24 19:02:13 +0530

    Add files via upload
    # Largest Digit Finder
    
    This Java program determines the largest digit in a given number. It iterates through each digit, comparing and finding the maximum value. The provided example uses the number 123, and outputs the largest digit 3. It's a simple yet effective demonstration of basic programming concepts like loops and conditionals.

---
+++ LargestDigit.java
@@ -0,0 +1,15 @@
+ class LargestDigit {
+    public static void main(String[] args) {
+        int number = 123;
+        int largestDigit = 0;
+       while (number > 0) {
+            int digit = number % 10;
+            if (digit > largestDigit) {
+                largestDigit = digit;
+            }
+            number = number / 10;
+        }
+
+        System.out.println("The largest digit is: " + largestDigit);
+    }
+}
