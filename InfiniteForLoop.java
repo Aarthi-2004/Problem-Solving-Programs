eb79f22d96484c2017188e48d56f03d37b9b8022
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-02-02 21:43:54 +0530

    Add files via upload

---
+++ InfiniteForLoop.java
@@ -0,0 +1,7 @@
+ class InfiniteForLoop {
+    public static void main(String[] args) {
+        for (;;) {
+            System.out.println("This is an infinite for loop");
+        }
+    }
+}
---
+++ InfiniteWhileLoop.java
@@ -0,0 +1,7 @@
+ class InfiniteWhileLoop {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println("This is an infinite while loop");
+        }
+    }
+}
