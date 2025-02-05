15770a618a59a8b414761381898b8db4067d8dd2
Aarthi.S <168005532+Aarthi-2004@users.noreply.github.com>
2025-02-05 19:09:46 +0530

    Add files via upload

---
+++ MirroredTrianglePattern.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+ class MirroredTrianglePattern {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the size: ");
+        int size = sc.nextInt();
+        sc.close();
+        for (int i = 1; i <= size; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = size - 1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
